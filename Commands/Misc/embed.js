ec6d69ea18bcdf7de7f272183a3dd5f74c24ded5
zNexusTTV <nexus2020onmk@gmail.com>
2024-02-08 22:00:10 +0100

    Update embed.js

--- Commands/Misc/embed.js
+++ Commands/Misc/embed.js
@@ -1,9 +1,9 @@
 const {
   Client,
   SlashCommandBuilder,
-  PermissionFlagsBits,
   ChannelType,
   EmbedBuilder,
+  PermissionsBitField,
 } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
@@ -41,39 +41,54 @@ module.exports = {
     .addStringOption((option) =>
       option.setName("footer").setDescription("Footer of the embed")
     )
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+    .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator),
   async execute(interaction, client) {
-    if (interaction.options.getAttachment("image") == null) {
-      interaction.options.getChannel("channel").send({
-        embeds: [
-          new EmbedBuilder()
-            .setTitle(`${interaction.options.getString("title") || " "}`)
-            .setDescription(
-              `${interaction.options.getString("description") || " "}`
-            )
-            .setColor(`${interaction.options.getString("color") || "Default"}`)
-            .setFooter({
-              text: `${interaction.options.getString("footer") || " "}`,
-            }),
-        ],
+    let channel = interaction.options.getChannel("channel");
+    let title = interaction.options.getString("title");
+    let description = interaction.options.getString("description");
+    let color = interaction.options.getString("color");
+    let image = interaction.options.getAttachment("image");
+    let footer = interaction.options.getString("footer");
+
+    let checkColor = /^#?([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/.test(color);
+    if (!checkColor) {
+      await interaction.reply({
+        content: "invalid embed color, embed sent anyway with color Deafult",
+        ephemeral: true,
       });
     } else {
-      interaction.options.getChannel("channel").send({
-        embeds: [
-          new EmbedBuilder()
-            .setTitle(`${interaction.options.getString("title") || " "}`)
-            .setDescription(
-              `${interaction.options.getString("description") || " "}`
-            )
-            .setImage(
-              `${interaction.options.getAttachment("image").attachment}`
-            )
-            .setColor(`${interaction.options.getString("color") || "Default"}`)
-            .setFooter({
-              text: `${interaction.options.getString("footer") || " "}`,
-            }),
-        ],
+      await interaction.reply({
+        content: "embed sent",
+        ephemeral: true,
       });
+    }
+    try {
+      if (image == null) {
+        channel.send({
+          embeds: [
+            new EmbedBuilder()
+              .setTitle(`${title || " "}`)
+              .setDescription(`${description || " "}`)
+              .setColor(`${checkColor ? color : "Default"}`)
+              .setTimestamp()
+              .setFooter({ text: `${footer || " "}` }),
+          ],
+        });
+      } else {
+        channel.send({
+          embeds: [
+            new EmbedBuilder()
+              .setTitle(`${title || " "}`)
+              .setDescription(`${description || " "}`)
+              .setColor(`${checkColor ? color : "Default"}`)
+              .setImage(`${image.attachment}`)
+              .setTimestamp()
+              .setFooter({ text: `${footer || " "}` }),
+          ],
+        });
+      }
+    } catch (err) {
+      console.log(err);
     }
   },
 };
